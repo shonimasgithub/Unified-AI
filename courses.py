22efbc97c13b1d9f0bfd1235254db5a2997ed144
SHONIMA S <158314891+shonimasgithub@users.noreply.github.com>
2025-07-07 22:12:47 +0530

    Add files via upload

---
+++ courses.py
@@ -0,0 +1,85 @@
+ds_courses = [
+    ["Machine Learning Crash Course by Google [Free]", "https://developers.google.com/machine-learning/crash-course"],
+    ["Machine Learning A-Z by Udemy", "https://www.udemy.com/course/machinelearning/"],
+    ["Machine Learning by Andrew NG", "https://www.coursera.org/learn/machine-learning"],
+]
+
+web_courses = [
+    ["Django Crash course [Free]", "https://youtu.be/e1IyzVyrLSU"],
+    ["Python and Django Full Stack Web Developer Bootcamp", "https://www.udemy.com/course/python-and-django-full-stack-web-developer-bootcamp"],
+    ["React Crash Course [Free]", "https://youtu.be/Dorf8i6lCuk"],
+]
+
+
+android_courses = [
+    ["Android Development for Beginners [Free]", "https://youtu.be/fis26HvvDII"],
+    ["Android App Development Specialization", "https://www.coursera.org/specializations/android-app-development"],
+]
+
+ios_courses = [
+    ["IOS App Development by LinkedIn", "https://www.linkedin.com/learning/subscription/topics/ios"],
+    ["iOS & Swift - The Complete iOS App Development Bootcamp", "https://www.udemy.com/course/ios-13-app-development-bootcamp/"],
+]
+
+uiux_courses = [
+    ["Google UX Design Professional Certificate", "https://www.coursera.org/professional-certificates/google-ux-design"],
+    ["UI / UX Design Specialization", "https://www.coursera.org/specializations/ui-ux-design"],
+]
+
+cloud_course = [
+    ['AWS Certified Solutions Architect by Amazon', 'https://aws.amazon.com/certification/certified-solutions-architect-associate/'],
+    ['Google Cloud Platform Fundamentals by Coursera', 'https://www.coursera.org/learn/gcp-fundamentals'],
+    ['Azure Fundamentals by Microsoft', 'https://docs.microsoft.com/en-us/learn/certifications/azure-fundamentals/'],
+    ['Cloud Computing Specialization by University of Illinois', 'https://www.coursera.org/specializations/cloud-computing'],
+    ['Cloud Computing and DevOps Program by Intellipaat', 'https://intellipaat.com/cloud-computing-devops-course/']
+]
+
+devops_course = [
+    ['DevOps Practitioner Certification by Simplilearn', 'https://www.simplilearn.com/devops-certification-training'],
+    ['Docker and Kubernetes: The Complete Guide by Udemy', 'https://www.udemy.com/course/docker-and-kubernetes-the-complete-guide/'],
+    ['Continuous Delivery & DevOps by Coursera', 'https://www.coursera.org/learn/continuous-integration'],
+    ['DevOps on AWS by Amazon', 'https://aws.amazon.com/training/devops/'],
+    ['Professional Certificate Program in Cloud Computing & DevOps by E&ICT Academy, IIT Kanpur', 'https://eicta.iitk.ac.in/product/professional-certificate-program-in-cloud-computing-devops-2/']
+]
+
+cybersecurity_course = [
+    ['Cybersecurity Specialization by University of Maryland', 'https://www.coursera.org/specializations/cyber-security'],
+    ['Introduction to Cyber Security by NYU', 'https://www.edx.org/course/intro-to-cyber-security'],
+    ['Certified Ethical Hacker (CEH) by EC-Council', 'https://www.eccouncil.org/programs/certified-ethical-hacker-ceh/'],
+    ['Cybersecurity Certification by Stanford University', 'https://online.stanford.edu/programs/cybersecurity-certificate'],
+    ['Fintech Overview: AI, Blockchain, Cloud, Data, Cybersecurity by Udemy', 'https://www.udemy.com/course/fintech-technologies-cloud-and-cybersecurity/']
+]
+
+data_engineering_course = [
+    ['Data Engineering on Google Cloud Platform by Coursera', 'https://www.coursera.org/specializations/gcp-data-engineering'],
+    ['Big Data Specialization by University of California San Diego', 'https://www.coursera.org/specializations/big-data'],
+    ['Data Engineering Nanodegree by Udacity', 'https://www.udacity.com/course/data-engineer-nanodegree--nd027'],
+    ['Data Engineering with AWS Machine Learning by edX', 'https://www.edx.org/course/data-engineering-with-aws-machine-learning'],
+    ['Data Engineering and Science at Razorpay', 'https://intellipaat.com/cloud-computing-devops-course/']
+]
+
+qa_testing_course = [
+    ['Software Testing and Automation Specialization by University of Minnesota', 'https://www.coursera.org/specializations/software-testing-automation'],
+    ['Selenium WebDriver with Java by Udemy', 'https://www.udemy.com/course/selenium-webdriver-with-java-basics-to-advanced/'],
+    ['Manual Testing Tutorial for Beginners by Guru99 [Free]', 'https://www.guru99.com/manual-testing.html'],
+    ['Test Automation University by Applitools [Free]', 'https://testautomationu.applitools.com/'],
+    ['Quality Assurance Certification by Simplilearn', 'https://www.simplilearn.com/quality-management/quality-assurance-certification-training']
+]
+
+blockchain_course = [
+    ['Blockchain Specialization by University at Buffalo', 'https://www.coursera.org/specializations/blockchain'],
+    ['Ethereum and Solidity: The Complete Developer’s Guide by Udemy', 'https://www.udemy.com/course/ethereum-and-solidity-the-complete-developers-guide/'],
+    ['Blockchain Developer Nanodegree by Udacity', 'https://www.udacity.com/course/blockchain-developer-nanodegree--nd1309'],
+    ['Bitcoin and Cryptocurrency Technologies by Princeton University', 'https://www.coursera.org/learn/cryptocurrency'],
+    ['Fintech Overview: AI, Blockchain, Cloud, Data, Cybersecurity by Udemy', 'https://www.udemy.com/course/fintech-technologies-cloud-and-cybersecurity/']
+]
+
+resume_videos = [
+    "https://youtu.be/y8YH0Qbu5h4",
+    "https://youtu.be/J-4Fv8nq1iA",
+]
+
+interview_videos = [
+    "https://youtu.be/Ji46s5BHdr0",
+    "https://youtu.be/seVxXHi2YMs",
+]
